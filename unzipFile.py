bddcb6afe4b3a240485ffcbe6495e9a483ba97db
xaiyu <727443133@qq.com>
2021-08-19 19:07:53 +0800

    Update unzipFile.py

--- unzipFile.py
+++ unzipFile.py
@@ -1,1 +1,10 @@
 
+import zipfile
+
+def unFile():
+    filezip = zipfile.ZipFile("./小容仔咕咕咕w - NO.05 白T.zip")
+    zipList = filezip.namelist()
+    for i in zipList:
+        filezip.extract(i, "./", pwd=b"leshe")
+    filezip.close()
+unFile()
